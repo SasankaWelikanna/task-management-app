1bfe2de92d22883d78048c775c7bb691b8d9ab62
SasankaWelikanna <sasankatw@gmail.com>
2024-05-11 15:21:37 +0530

    Splash screen

---
+++ app/src/main/java/com/example/taskapp/SplashScreen.kt
@@ -0,0 +1,34 @@
+package com.example.taskapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashScreen : AppCompatActivity() {
+
+    lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        supportActionBar?.hide()
+
+        handler = Handler()
+
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 4000)
+    }
+
+}
